c08539f02a0ebfe3d7907b5f96d1057dc2e62ec3
FourierYe <939647181@qq.com>
2022-11-20 22:46:51 +0800

    sync

--- Lecture/Lecture56/code/generate_face.py
+++ Lecture/Lecture56/code/generate_face.py
@@ -1,7 +1,7 @@
 import scipy.io as scio
 data = scio.loadmat('data_for_labC.mat')
 eignfaces_blk = data['eignfaces_blk']
-db = data["employees_DB"]
 
-for i in range(len(db)):
-    print(db[i])
+db = data["employees_DB"]
+print(f"ID: {db[0][0][0]}")
+print(f"weight: {db[0][0][1].shape}")
